9cb87d59034975c4d1a160ea4156d5c301a36e4b
drzix <korea.drzix@gmail.com>
2016-01-12 21:25:44 +0900

    배운거
    Signed-off-by: drzix <korea.drzix@gmail.com>

---
+++ 160111/search.py
@@ -0,0 +1,11 @@
+def mysearch(ls, inp):
+    for n in range(0, len(ls)): # [0, len(ls))
+        if ls[n] == inp:
+            return n
+
+    return None
+
+mylist = [1, 3, 5, 7, 9]
+mylist.sort()
+userinput = int(raw_input("Enter the number you want to find: "))
+print mysearch(ls=mylist, inp=userinput)
